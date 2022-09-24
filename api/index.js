143a76ac42617cf2c12a45d55618dbeb813a4a91
David Huang <huangd1224@gmail.com>
2022-09-24 17:53:54 -0400

    idk

--- api/index.js
+++ api/index.js
@@ -18,16 +18,4 @@ app.use(bodyParser.json())
 const router = require('./routes/router')
 app.use('/api', router)
 
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-const http = require('http');
-// const express = require('express');
-
-// const route = require('./routes');
-// const app = express();
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(route); app.use('/', (req, res,next)=>{
-//    res.send('<h1> first midleware: Hello Tutorials Point </h1>');
-// });
-const server = http.createServer(app);
-server.listen(PORT);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
