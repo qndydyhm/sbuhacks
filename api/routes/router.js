ee6c5fd5f1a335ed8086f79b798c36d89f1c80a2
Renge <renge@renge.io>
2022-09-24 21:51:39 -0400

    feat: implement logOutUser

--- api/routes/router.js
+++ api/routes/router.js
@@ -4,5 +4,6 @@ const UserAPI = require('../apis/user-apis')
 
 router.post('/login', UserAPI.loginUser)
 router.post('/register', UserAPI.registerUser)
+router.get('/logout', UserAPI.logoutUser)
 
 module.exports = router
