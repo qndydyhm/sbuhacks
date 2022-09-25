cd493da3e8a1e40e61bbeb230d95b7e734151b16
Renge <renge@renge.io>
2022-09-24 20:23:26 -0400

    fix: fix login logic

---
+++ auth/auth/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const config = require('../config')
+
+function authManager() {
+    getUserID = function (token) {
+        try {
+            if (!token) {
+                return 'Guest';
+            }
+            else {
+                const verified = jwt.verify(token, config.JWT_SECRET)
+                return verified.userId
+            }
+        } catch (err) {
+            return 'Guest'
+        }
+    }
+
+    signToken = function (user) {
+        return jwt.sign({
+            userId: user._id
+        }, config.JWT_SECRET);
+    }
+
+    return this;
+}
+
+const auth = authManager();
+module.exports = auth;
