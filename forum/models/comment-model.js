83657a8a71b759766b03cf223f78151523e8e30b
Renge <renge@renge.io>
2022-09-24 23:27:21 -0400

    fix: commentschema

--- forum/models/comment-model.js
+++ forum/models/comment-model.js
@@ -2,18 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
-const ForumSchema = new Schema(
+const CommentSchema = new Schema(
     {
-        title: { type: String, required: true },
-        author: { type: ObjectId, required: true },
-        category: { type: Boolean, required: true },
-        tags: { type: [String], required: true },
-        images: { type: [ObjectId], required: true },
-        content: { type: String, required: true }, 
-        comments: { type: [ObjectId], required: true },
-        favorited_by: { type: Number, required: true },
+        forum: {type: ObjectId, require: true},
+        author: {type: ObjectId, require: true},
+        content: {type: String, require: true}
     },
     { timestamps: true }
 )
 
-module.exports = mongoose.model('ForumSchema', ForumSchema);
+module.exports = mongoose.model('CommentSchema', CommentSchema);
